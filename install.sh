d39a595b62b904a26b324e4c6235b8d2126546db
olliejohnson <oliver.joseph.johnson@gmail.com>
2022-01-08 22:39:25 -0500

    Update Scripts

--- install.sh
+++ install.sh
@@ -16,3 +16,7 @@ sudo certbot certonly --nginx
 sudo certbot renew --dry-run
 
 curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+
+mkdir -p /etc/pterodactyl
+curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+chmod u+x /usr/local/bin/wings
